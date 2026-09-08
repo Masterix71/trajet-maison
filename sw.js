d88387883d961073b1cd46c1fae0a60fcede7561
Masterix71 <87244186+Masterix71@users.noreply.github.com>
2026-09-08 22:54:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'trajet-cruas-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
